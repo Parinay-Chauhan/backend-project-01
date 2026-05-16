334516a7690d5166e5286b7efa502c86d9e5b6a9
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-05-16 12:44:55 +0530

    create schema

--- aap.js
+++ aap.js
@@ -1,11 +1,18 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const cookiesParser = require("cookie-parser");
+const path = require("path");
 
-app.get('/', (req, res)=> {
-    res.send('Hello World');
-})
+app.set("view engine", "ejs");
+app.use(cookiesParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
 
 app.listen(PORT);
